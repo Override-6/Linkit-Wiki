799994436f34bfa40f1df7d6b72af92aac3a29f6
Maxime <maximebatista18@gmail.com>
2022-07-14 16:17:54 +0200

    enhancing doc

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -95,7 +95,7 @@ const config = {
                         ],
                     },
                                     ],
-                copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+                copyright: `Copyright © ${new Date().getFullYear()} Linkit, Inc.<br>Built with Docusaurus.`,
             },
             prism: {
                 theme: lightCodeTheme,
---
+++ src/components/pages.js
@@ -0,0 +1,22 @@
+
+const pages = {
+    getting_started: {
+        intro: "/Linkit/docs/Getting Stared/Naming/Introduction",
+        framework_architecture: "/Linkit/docs/Getting Stared/Naming/Framework Architecture",
+        first_network: "/Linkit/docs/Getting Stared/Naming/Your First Network",
+    },
+    gnom: {
+        naming: {
+            intro: "/Linkit/docs/GNOM/Naming/Introduction",
+            nol: "/Linkit/docs/GNOM/Naming/Network Object Linkers",
+            nor: "/Linkit/docs/GNOM/Naming/Network Object Reference",
+            no: "/Linkit/docs/GNOM/Naming/What is a network object",
+        },
+        caches: {
+            connected_objects: {
+                bhv_lang: {}
+
+            }
+        }
+    }
+}
