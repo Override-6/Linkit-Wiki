43972ac6029dd369a56421e2aa549472edcfb286
Maxime <maximebatista18@gmail.com>
2022-09-02 23:21:55 +0200

    starting workers placement generation

--- src/theme/DocPage/Layout/Main/index.js
+++ src/theme/DocPage/Layout/Main/index.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useRef, useState} from 'react';
 import Main from '@theme-original/DocPage/Layout/Main';
+import "index.css"
 
 const svgsDir = "/src/components/diagrams/workers/svgs/"
 const NearWallSvgs = ["cables", "dig", "hammer", "paint", "perçeuse", "pique-sol", "souder"]
@@ -7,8 +8,10 @@ const Middle1 = ["pique-sol", "reader", "Brouette", "dig", "wallmaker"]
 const Middle2 = ["pique-sol", "reader", "Brouette", "dig"]
 const Middle3 = ["reader", "wallmaker", "Brouette"]
 
-function genWorkers() {
-    return [new Worker(NearWallSvgs, false), new Worker(Middle1, false), new Worker(Middle2), new Worker(Middle3), new Worker(Middle2), new Worker(Middle1, true), new Worker(NearWallSvgs, true)]
+function getWorkers() {
+    return <div id={"Workers"}>
+
+    </div>
 }
 
 function Header() {
@@ -21,23 +24,16 @@ function Footer() {
     return <></>
 }
 
-class Worker {
-
-    constructor(svgs, reversed) {
-        this.spawns = Math.random() > 1 / 3
-        if (reversed === undefined) this.reversed = Math.random() > 0.5
-        else this.reversed = reversed
-        if (this.spawns) {
-            this.svg = svgsDir + svgs[Math.floor(Math.random() * svgs.length) + 1] + ".svg"
-        }
+function Worker({svgs, reverse}) {
+    const spawns = Math.random() > 1 / 3
+    let reversed = reverse
+    if (reversed === undefined) reversed = Math.random() > 0.5
+    if (spawns) {
+        const svg = svgsDir + svgs[Math.floor(Math.random() * svgs.length) + 1] + ".svg"
+        if (reversed) return <img src={svg} alt={svg} style={{transform: "scaleX(-1)"}}/>
+        return <img src={svg} alt={svg}/>
     }
-
-
-    render() {
-        if (!this.spawns) return <></>
-        if (this.reversed) return <img src={this.svg} alt={svg} style={{transform: "scaleX(-1)"}}/>
-        return <img src={this.svg} alt={svg}/>
-    }
+    return <></>
 }
 
 export default function MainWrapper(props) {
